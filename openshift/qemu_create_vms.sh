eec024b3a6306d6a654d43d1d51f148de22852c9
khramov86 <khramov.vladimir@gmail.com>
2021-07-08 12:44:05 +0300

    Create qemu_create_vms.sh

---
+++ openshift/qemu_create_vms.sh
@@ -0,0 +1,32 @@
+#/usr/bin/env bash
+declare -A INVENTORY
+ID_GROUP=200
+#let IDGROUP++
+function counter()
+{
+        let ID_GROUP++
+        echo $ID_GROUP
+}
+INVENTORY=( \
+[mas1]="DE:89:EB:84:82:25,$(( ID_GROUP + 1 ))" \
+[mas2]="26:B0:96:2E:68:FF,$(( ID_GROUP + 2 ))" \
+[mas3]="28:A5:67:4B:36:DF,$(( ID_GROUP + 3 ))" \
+[work1]="09:D7:96:2D:88:EF,$(( ID_GROUP + 4 ))" \
+[work2]="02:D8:49:1B:46:EE,$(( ID_GROUP + 5 ))" \
+[boot]="36:25:A6:89:2E:B1,$(( ID_GROUP + 9 ))" \
+)
+VLANID=2000
+for host in ${!INVENTORY[@]}
+  do
+    hostid=`echo ${INVENTORY[$host]} |awk -F\, '{print $2}'`
+    hostmac=`echo ${INVENTORY[$host]} |awk -F\, '{print $1}'`
+    hostlist="$(qm list |awk '{print $1}')"
+      if [[ "$hostlist" =~ "$hostid" ]]; then
+        echo "VM exists, skipping"
+      else
+        echo "Creating VM"
+        qm clone 5002 $hostid --full=yes --name="oc-$host"
+      fi
+    echo "Setting VLANs"
+    qm set $hostid --net0 virtio,firewall=0,macaddr=${hostmac},tag=${VLANID}
+done
